48d14863ac4fd9103f42abd8968a948e8b0a900b
Karl Vincent <karl_vincent_salvacion@dlsl.edu.ph>
2025-03-18 23:24:07 +0800

    changed names

--- components/AddMedicineLanding.tsx
+++ components/AddMedicineLanding.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamList } from "../navigation/NavigationTypes"; 
+import { RootStackParamList } from "../navigation/navigationTypes"; 
 import { useMedicine } from "../context/MedicineContext";
 import { Feather, AntDesign } from "@expo/vector-icons";
 import stylesAddMed from "../style-components/StylesAddMed";
